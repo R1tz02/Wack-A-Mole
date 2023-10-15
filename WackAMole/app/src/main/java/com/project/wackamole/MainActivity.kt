314d7bcaae42fc414957f60d811d935b571cb1eb
Jack Johnson <jaxsterbaxster2@gmail.com>
2023-10-15 14:42:18 -0500

    remove toast

--- WackAMole/app/src/main/java/com/project/wackamole/MainActivity.kt
+++ WackAMole/app/src/main/java/com/project/wackamole/MainActivity.kt
@@ -145,8 +145,6 @@ class MainActivity : AppCompatActivity() {
             override fun onFinish() {
                 game.gameOver()
                 setMole()
-                val text = getString(R.string.gameOver, game.getScore())
-                Toast.makeText(applicationContext , text, Toast.LENGTH_LONG).show()
                 newGameButt.isClickable = true
                 newGameButt.alpha = 1F
             }
